1fba57b9cbd9793e72fd71b1ebf9f9ab3e502709
Dogged-Coder <dogged.coder@gmail.com>
2026-06-26 11:53:17 +0530

    26th

--- 26th_June.py
+++ 26th_June.py
@@ -1,3 +1,30 @@
+#Tricky 
+#what i though was a loooot complex method
+#REMEMBER ALL METHODS OF DATA TYPES
+"""
+58. Length of Last Word
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+"""
 class Solution(object):
     def lengthOfLastWord(self, s):
         return len(s.split()[-1])
